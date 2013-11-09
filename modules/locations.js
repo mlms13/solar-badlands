73e9bac0e12a2d0b111fb1079769048b4f793ee7
Jason <jcvalle@mail.usf.edu>
2013-11-09 17:42:04 -0500

    defining locations

--- modules/locations.js
+++ modules/locations.js
@@ -2,22 +2,56 @@ var locations = {};
 
 locations.earth = {
     room : {
-        message: "You wake up in YOUR ROOM. It is  nice day out. There are some books to the WEST, and STAIRS to the SOUTH.",
-    //     actions: {
-    //         'eat alien eggs': {
-    //             synonyms: ['consume', 'eat eggs'],
-    //             fn: function (user) {
+        message: "You wake up in YOUR ROOM to a loud crash. It is a nice day out. There are some books to the WEST, and STAIRS to the SOUTH.",
+        actions: {
+            'get': {
+                fn: function (user, text, callback) {
+                    if (text.toLowerCase().indexOf('books') > -1) {
+                        callback(null, 'These sure do look interesting, but you don\'t have any use for them now.');
+                    } else {
+                        callback(null, 'You can\'t GET that. Try doing something else, or asking for HELP.');
+                    }
+                }
+            },
+            'go': {
+                fn: function (user, text, callback) {
+                    if (text.toLowerCase().indexOf('stairs') > -1) {
+                        callback(locations.earth.downstairs);
+                    } else {
+                        callback(null, 'You can\'t GO there.  It either isn\'t a place, or you aren\'t where you think you are.');
+                    }
+                }
+            }
+        }
+    },
+    downstairs : {
+        message: "You are now DOWNSTAIRS. MOM is in the kitchen, there is a table with chairs to the EAST and a DOOR to the SOUTH.",
+        actions: {
+            'get': {
+                fn: function (user, text, callback) {
+                    callback(null, 'There isn\'t anything to GET here. Need HELP? You just might...');
+                }
+            },
+            'talk': {
+                fn: function (user, text, callback) {
+                    if (text.toLowerCase().indexOf('mom') > -1) {
+                        //update freakin inventory
 
-    //             }
-    //         }
-    //         'go': {
-    //             fn: function (user) {
-
-    //             }
-    //         }
-    //     }
+                        callback(null, 'You got a SANDWICH, and JUICE BOX from MOM. USE these when your health gets low. They have been added to your INVENTORY.');
+                    }
+                }
+            },
+            'go': {
+                fn: function (user, text, callback) {
+                    if (text.toLowerCase().indexOf('door') > -1) {
+                        callback(locations.earth.frontyard);
+                    } else {
+                        callback(null, 'That isn\'t a real place. Or it isn\'t a fake place. Matter of perspective.');
+                    }
+                }
+            }
+        }
     }
-    
 };
 
 module.exports = locations;
