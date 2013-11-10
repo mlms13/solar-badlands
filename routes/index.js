6f1a755e66bb7432e50e68783fc0258a0d9398ba
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-09 21:23:28 -0500

    Add error handling to the index page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var db = require('../modules/db.js');
+
 // GET home page
 exports.render = function(req, res) {
     res.render('index');
@@ -8,5 +10,15 @@ exports.submit = function (req, res) {
     var handle = req.param('tw-handle');
     handle = handle.indexOf("@") === 0 ? handle.slice(1) : handle;
 
-    res.redirect('/user/' + handle);
+    db.getUser(handle, function (err, user) {
+        if (err) {
+            res.render('index', {error: {title: "Troubles!", body: "We had some problems connecting to the database."}});
+            return;
+        }
+        if (user) {
+            res.redirect('/user/' + handle);
+        } else {
+            res.render('index', {error: {title: "User not found!", body: "Are you sure the user has started a game?"}});
+        }
+    });
 }
