f9be8d4363a4041f8f67c569f50ac40693f04e0b
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-10 13:55:05 -0500

    Keep track of a duplicate count
    Reset it when the user tweets at us. Only keep trying to tweet back if
    the count is below 5.

--- clients/twitter.js
+++ clients/twitter.js
@@ -12,6 +12,8 @@ var T = new Twit({
 
 var stream = T.stream('user', {'with': 'user'});
 
+var duplicateCount = 0;
+
 var getDupeMessageString = function () {
     // Twitter doesn't like sending the same message over and over again
     // so the user tweeting "LOOK AROUND" can be problematic if we recently posted the same thing to them.
@@ -21,7 +23,14 @@ var getDupeMessageString = function () {
         "The knowledge you seek is already available to you. In your Twitter feed.",
         "Think hard, or maybe just look a little harder. The information has already been given to you.",
         "I'm not in the mood to repeat myself. Are you in the mood to use your scroll wheel?",
-        "If you make me repeat myself too much, Twitter may think I'm just a bot."
+        "If you make me repeat myself too much, Twitter may think I'm just a bot.",
+        "I remember saying that recently. I'm sure you'll find it if you look through your feed.",
+        "I just said that pretty recently. It has to be there in your feed somewhere...",
+        "Check out your feed. The information you seek has already be given to you.",
+        "If you're not careful, I'm going to start repeating myself.",
+        "I already told you this once, it's in your Twitter feed.",
+        "Scroll through your Twitter feed, the information you seek has been given already.",
+        "Look through your feed. You'll find my response somewhere without me needing to repeat myself."
     ];
 
     // return a random message
@@ -32,11 +41,18 @@ var postMessage = function (replyMessage, callback) {
     T.post('statuses/update', replyMessage, function (err, reply) {
         // if twitter is complaining about a duplicate message
         if (err && err.code === 187) {
+            // increase the duplicate count
+            duplicateCount += 1;
             console.warn("Our message was a duplicate, so we're saying something else instead.");
-            // remove everything after the first space and replace it with our new message
-            replyMessage.status = replyMessage.status.substring(0, replyMessage.status.indexOf(' ') + 1) + getDupeMessageString();
-            // then try to re-post
-            postMessage(replyMessage);
+
+            // only try posting if the duplicate count is less than 5
+            // so we don't keep spamming the server with nothing but duplicate messages
+            if (duplicateCount < 5) {
+                // remove everything after the first space and replace it with our new message
+                replyMessage.status = replyMessage.status.substring(0, replyMessage.status.indexOf(' ') + 1) + getDupeMessageString();
+                // then try to re-post
+                postMessage(replyMessage);
+            }
             return;
         } else if (err) {
             // if the error isn't related to duplicate messages, log it
@@ -59,6 +75,9 @@ module.exports.startClient = function () {
                 if (err) {
                     response = {name: "Looks like we're having some problems. Try again in a bit, or tweet at @mlms13 for support."};
                 }
+
+                // reset the duplicate counter
+                duplicateCount = 0;
 
                 // send `response` to user
                 postMessage({ in_reply_to_status_id: tweet.id_str, status: '@' + tweet.user.screen_name + ' ' + response.text }, function (err, reply) {
