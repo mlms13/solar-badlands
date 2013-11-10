d2ff2ba37606e20bc54f8b5bc32232a78b56cb57
Jason <jcvalle@mail.usf.edu>
2013-11-10 14:45:04 -0500

    more location stuff

--- modules/locations.js
+++ modules/locations.js
@@ -4,6 +4,11 @@ locations.earth = {
     room : {
         message: {text: "You wake up in YOUR ROOM to a loud crash. It is a nice day out. There are some books to the WEST, and STAIRS to the SOUTH."},
         actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'You are in YOUR ROOM. It is probably time to start heading DOWNSTAIRS.'});
+                }
+            },
             'get': {
                 fn: function (user, text, callback) {
                     if (text.indexOf('books') > -1) {
@@ -32,6 +37,11 @@ locations.earth = {
     downstairs : {
         message: {text: "You are now DOWNSTAIRS. MOM is in the kitchen, there is a table with chairs to the EAST and a DOOR to the SOUTH."},
         actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'You are DOWNSTAIRS. MOM is cooking something, but its always nice to talk to her. There is a DOOR to the SOUTH.'});
+                }
+            },
             'get': {
                 fn: function (user, text, callback) {
                     callback(null, {text: 'There isn\'t anything to GET here. Need HELP? You just might...'});
@@ -65,6 +75,11 @@ locations.earth = {
     frontyard: {
         message: {text: 'You are in the FRONTYARD. There\'s a fence to the SOUTH and a large SPACESHIP to the EAST. Wait, that wasn\'t here yesterday.'},
         actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'Not much of interest going on in the FRONTYARD except that gigantic SPACESHIP.'});
+                }
+            },
             'get': {
                 fn: function (user, text, callback) {
                     callback(null, {text: 'There\'s nothing to get here. See what\'s up with that SPACESHIP over there.'});
@@ -95,11 +110,16 @@ locations.earth = {
     spaceship: {
         message: {text: 'You are now inside the SPACESHIP! There is a DASHBOARD in front of you and what looks like LOCKERS to the RIGHT.'},
         actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'You are inside the SPACESHIP! The DASHBOARD looks overwhelming. Are those LOCKERS to the RIGHT?'});
+                }
+            },
             'push': {
                 synonyms: ['push', 'press'],
                 fn: function (user, text, callback) {
                     if (text.indexOf('red') > -1) {
-                        callback({area: 'spaceshipAlpha', level: 'level1'});
+                        callback({area: 'spaceshipAlpha', level: 'earthOrbit'});
                     } else if (text.indexOf('blue') > -1) {
                         // TODO this is where we need to call a you're dead function and explode the freakin ship and show a picture of the freakin exploded dead ship
                         callback(null, {text: 'BOOOOOOOOOOOM!!!!!!! YOU\'RE FREAKIN DEAD!!!!!!!!!!'});
@@ -111,7 +131,7 @@ locations.earth = {
             'look at': {
                 fn: function (user, text, callback) {
                     if (text.indexOf('dashboard') > -1) {
-                        callback(null, {text: 'There are buttons and controls all over the place. A RED and BLUE button is glowing.'});
+                        callback(null, {text: 'There are buttons and controls all over the place. A big touchscreen and A RED and BLUE button is glowing.'});
                     } else if (text.indexOf('lockers') > -1) {
                         callback(null, {text: 'Some futuristic looking LOCKERS. I wonder if there\'s anything useful inside?'});
                     } else {
@@ -156,7 +176,7 @@ locations.earth = {
                     if (text.indexOf('space') > -1) {
                         callback(null, {text: 'How high can you jump? Probably not high enough to get to SPACE, man. Find another way.'});
                     } else {
-                        callback(null, {text: 'There isn\'t really anywhere you can GO from here. Maybe see what this SPACESHIP is capable of.'})
+                        callback(null, {text: 'There isn\'t really anywhere you can GO from here. Maybe see what this SPACESHIP is capable of.'});
                     }
                 }
             }
@@ -165,13 +185,107 @@ locations.earth = {
 };
 
 locations.spaceshipAlpha = {
-    level1 : {
+    earthOrbit : {
         message: {text: 'The DOOR slams shut! Everything starts to rumble and you are pasted to the floor. Suddenly... you\'re in SPACE!'},
         actions: {
-            'look': {
-                synonyms: ['look'],
+            'look around': {
                 fn: function (user, text, callback) {
-                    callback(null, {text: 'Sweet. Space.'});
+                    callback(null, {text: 'Outside is SPACE. A DASHBOARD is in front of you and the LOCKERS are still here too. The DOOR is sealed behind you.'});
+                }
+            },
+            'look at': {
+                fn: function (user, text, callback) {
+                    if (text.indexOf('dashboard') > -1) {
+                        callback(null, {text: 'BUTTONS all over the place. A MONITOR with KEYBOARD is here that appears to be in English... Convenient...'});
+                    } else if (text.indexOf('monitor') > -1) {
+                        callback(null, {text: 'The monitor reads: \"Last Location: Earth-Moon, Current Location: Earth-Orbit, Destination: _\". And the cursor is blinking.'});
+                    } else if (text.indexOf('keyboard') > -1) {
+                        callback(null, {text: 'Not a standard English keyboard, but there are definitely some recognizable characters here. Can you TYPE?'});
+                    } else if (text.indexOf('locker') > -1) {
+                        callback(null, {text: 'The same lockers as before. That SAFE is still here, and a couple of spare SPACESUITS.'});
+                    } else if (text.indexOf('door') > -1) {
+                        callback(null, {text: 'This DOOR is sealed shut. There\'s a LEVER next to it, but it might not be best to PULL that out here.'});
+                    }
+                }
+            },
+            'type': {
+                synonyms: ['type'],
+                fn: function (user, text, callback) {
+                    if (text.indexOf('moon') > -1) {
+                        callback({area: 'moon', level: 'spaceshipClosed'});
+                    } else if (text.indexOf('earth') > -1) {
+                        callback(null, {text: 'Really? Had enough of SPACE already? GAME OVER man, you lose. Okay not really, try a different place though.'});
+                    }
+                    else {
+                        callback(null, {text: 'The SPACESHIP didn\'t seem to recognize that input... Maybe try something its familiar with?'});
+                    }
+                }
+            },
+            'pull': {
+                synonyms: ['pull'],
+                fn: function (user, text, callback) {
+                    if (text.indexOf('lever') > -1) {
+                        callback(null, {text: 'You try to PULL the LEVER, but it doesn\'t budge. That is a nice feature, be thankful.'});
+                    } else {
+                        callback(null, {text: 'There is only one thing here you can pull, currently. This wasn\'t it...'});
+                    }
+                }
+            },
+            'get': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'There\'s nothing to get here, homeslice.'});
+                }
+            },
+            'go': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'This is a one-room spaceship, there isn\'t really anywhere to go. Try interacting with somethingn else.'});
+                }
+            }
+        }
+    }
+};
+
+locations.moon = {
+    spaceshipClosed: {
+        message: {text: 'The ship speeds up and throws you to the floor again. After some rumbling, it appears we have landed somewhere.'},
+        actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: 'Looks like you\'ve landed on the moon. The door behind you '});
+                }
+            },
+            'pull': {
+                synonyms: ['pull'],
+                fn: function (user, text, callback) {
+                    if (text.indexOf('lever') > -1) {
+                        callback({area: 'moon', level: 'spaceshipOpen'});
+                    } else {
+                        callback(null, {text: 'There is only one thing here you can pull, currently. This wasn\'t it...'});
+                    }
+                }
+            },
+            'go': {
+                fn: function (user, text, callback) {
+                    if (text.indexOf('door') > -1) {
+                        callback(null, {text: 'The DOOR still seems to be locked, maybe that LEVER next to it has something to do with that...'});
+                    } else {
+                        callback(null, {text: 'This is a one-room spaceship, there isn\'t really anywhere to go. Try interacting with somethingn else.'});
+                    }
+                }
+            }
+        }
+    },
+    spaceshipOpen: {
+        message: {text: 'Sounds like some pressure was released. And the DOOR slides open. '},
+        actions: {
+            'look around': {
+                fn: function (user, text, callback) {
+                    callback(null, {text: ''});
+                }
+            },
+            '': {
+                fn: function(user, text, callback) {
+
                 }
             }
         }
