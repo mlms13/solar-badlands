4d75388cb7e8e3e3924970baff862a5b0b535dad
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-10 17:09:36 -0500

    Home page doesn't need to handle posting

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,3 @@ var db = require('../modules/db.js');
 exports.render = function(req, res) {
     res.render('index');
 };
-
-// POST to home page
-exports.submit = function (req, res) {
-    var handle = req.param('tw-handle');
-    handle = handle.indexOf("@") === 0 ? handle.slice(1) : handle;
-
-    db.getUser(handle, function (err, user) {
-        if (err) {
-            res.render('index', {error: {title: "Troubles!", body: "We had some problems connecting to the database."}});
-            return;
-        }
-        if (user) {
-            res.redirect('/user/' + handle);
-        } else {
-            res.render('index', {error: {title: "User not found!", body: "Are you sure the user has started a game?"}});
-        }
-    });
-}
